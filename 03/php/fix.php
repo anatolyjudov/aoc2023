d897709546aee43f098559e5c435162952cd7975
Anatoliy Yudov <anatoliy.yudov@supermetrics.com>
2023-12-03 09:51:12 +0200

    03: php solution

---
+++ 03/php/fix.php
@@ -0,0 +1,67 @@
+<?php
+
+$inputLines = file('input.txt', FILE_IGNORE_NEW_LINES);
+
+$numbers = $symbols = $gears = [];
+$sum = $ratiosSum = 0;
+
+for ($y = 0; $y < sizeof($inputLines); $y++) {
+    $inputLine = $inputLines[$y] . '.';
+    $number = '';
+
+    for($x = 0; $x < strlen($inputLine); $x++) {
+        $next = $inputLine[$x];
+
+        if (ord($next) >= 48 && ord($next) <= 57) {
+            $number .= $next;
+            continue;
+        }
+
+        if ($number !== '') {
+            $numbers[] = [
+                'value' => $number,
+                'y' => $y,
+                'x1' => $x - strlen($number),
+                'x2' => $x - 1
+            ];
+            $number = '';
+        }
+
+        if ($next !== '.') {
+            $symbols[$y][$x] = $next;
+        }
+    }
+}
+
+foreach ($numbers as $numberId => $number) {
+    $symbolFound = false;
+
+    for($y = $number['y'] - 1; $y <= $number['y'] + 1; $y++) {
+
+        for($x = $number['x1'] - 1; $x <= $number['x2'] + 1; $x++) {
+
+            if (isset($symbols[$y][$x])) {
+                $symbolFound = true;
+            } else continue;
+
+            if (($symbols[$y][$x] === '*') && !isset($gears["$y:$x"][$numberId])) {
+                $gears["$y:$x"][$numberId] = $number['value'];
+            }
+        }
+    }
+
+    $sum += $symbolFound ? $number['value'] : 0;
+}
+
+foreach ($gears as $gear) {
+    if (sizeof($gear) === 2) {
+        $ratiosSum += array_product($gear);
+    }
+}
+
+print_r($symbols);
+print_r($numbers);
+print_r($gears);
+
+echo $sum . "\r\n";
+echo $ratiosSum . "\r\n";
