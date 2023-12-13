adfb97cb974aa482372b6ce5edddf39f07788cbb
Anatoliy Yudov <anatoly.judov@gmail.com>
2023-12-13 11:30:09 +0200

    13: php solution 1

--- 13/php/reflect.php
+++ 13/php/reflect.php
@@ -3,7 +3,7 @@
 $inputLines = file('input.txt', FILE_IGNORE_NEW_LINES);
 
 $patterns = [];
-$p = 0;
+$p = $sum = 0;
 
 foreach ($inputLines as $inputLine) {
     if ($inputLine === '') {
@@ -14,43 +14,34 @@ foreach ($inputLines as $inputLine) {
     $patterns[$p][] = $inputLine;
 }
 
-$pats = [];
-foreach ($patterns as $pattern) {
-    $hors = [];
+foreach ($patterns as $pattern_num => $pattern) {
+    $p = ['h' => [], 'v' => []];
+    
     foreach ($pattern as $line) {
-        $hors[] = bindec(str_replace(['.', '#'], ['0', '1'], $line));
+        $p['h'][] = bindec(str_replace(['.', '#'], ['0', '1'], $line));
+
     }
-    $vers = [];
+
     $rotated = rotate($pattern);
     foreach ($rotated as $line) {
-        $vers[] = bindec(str_replace(['.', '#'], ['0', '1'], implode('', $line)));
+        $p['v'][] = bindec(str_replace(['.', '#'], ['0', '1'], implode('', $line)));
     }
-    $pats[] = [
-        'hors' => $hors,
-        'vers' => $vers
-    ];
-}
 
-//print_r($pats);
-
-$sum = 0;
-foreach ($pats as $pat) {
-    foreach ($pat as $type => $h) {
-        for ($i = 1; $i < sizeof($h) - 1; $i++) {
-            $slice1 = array_reverse(array_slice($h, 0, $i + 1));
-            $slice2 = array_slice($h, $i + 1);
+    foreach ($p as $type => $codes) {
+        for ($i = 0; $i < sizeof($codes) - 1; $i++) {
+            $slice1 = array_reverse(array_slice($codes, 0, $i + 1));
+            $slice2 = array_slice($codes, $i + 1);
             $size = min(sizeof($slice1), sizeof($slice2));
             $slice1 = array_slice($slice1, 0, $size);
             $slice2 = array_slice($slice2, 0, $size);
             if ($slice1 === $slice2) {
-                $sum += ($i + 1) * (($type === 'hors') ? 100 : 1);
+                $sum += ($i + 1) * (($type === 'h') ? 100 : 1);
             }
         }
     }
 }
 
 echo $sum;
-
 
 function rotate($arr)
 {
