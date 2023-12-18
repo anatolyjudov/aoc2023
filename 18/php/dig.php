182e2ceee01e1e092a436fb61d12eee19fbb1061
Anatoliy Yudov <anatoly.judov@gmail.com>
2023-12-18 15:44:46 +0400

    18: 1

--- 18/php/dig.php
+++ 18/php/dig.php
@@ -13,12 +13,12 @@ foreach($inputLines as $n => $line) {
     $data = explode(' ', $line);
     $trenches[$n] = [
         'dir' => $data[0],
-        'dist' => $data[1] * 10000,
+        'dist' => $data[1],
         'rgb' => str_split(substr($data[2], 2, -1), 2),
         'start' => $start,
     ];
-    $end[0] = $start[0] + match($data[0]) {'U' => -1, 'D' => 1, 'L', 'R' => 0} * $data[1] * 10000;
-    $end[1] = $start[1] + match($data[0]) {'U', 'D' => 0, 'L' => -1, 'R' => 1} * $data[1] * 10000;
+    $end[0] = $start[0] + match($data[0]) {'U' => -1, 'D' => 1, 'L', 'R' => 0} * $data[1];
+    $end[1] = $start[1] + match($data[0]) {'U', 'D' => 0, 'L' => -1, 'R' => 1} * $data[1];
     $trenches[$n]['end'] = $end;
 
     $Ymax = max($Ymax, $end[0], $start[0]);
@@ -30,7 +30,6 @@ foreach($inputLines as $n => $line) {
     $start = $end;
 }
 
-/*
 foreach($trenches as $n => $trench) {
     for($y = min($trench['start'][0], $trench['end'][0]); $y <= max($trench['start'][0], $trench['end'][0]); $y = $y + 1) {
         for($x = min($trench['start'][1], $trench['end'][1]); $x <= max($trench['start'][1], $trench['end'][1]); $x = $x + 1) {
@@ -38,8 +37,6 @@ foreach($trenches as $n => $trench) {
         }
     }
 }
-*/
-
 
 // print
 /*
